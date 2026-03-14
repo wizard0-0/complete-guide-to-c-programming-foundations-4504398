b3f731307666c51ae6bd99b6e327ef3b51847374
Your Name <hassanrizwan180@gmail.com>
2026-03-14 22:45:06 +0500

    14 march if-loops

--- CH04/04_09/04_09-challenge1.c
+++ CH04/04_09/04_09-challenge1.c
@@ -2,12 +2,21 @@
 
 int main()
 {
-	int a;
+	int a,loop=0;
 
 	printf("How long is the line? ");
 	scanf("%d",&a);
 
 	/* write the loop here */
+// for (int i=0; i<a;i++)
+// {
+// 	putchar('-');
+// }
+while (loop<a)
+{
+	putchar('-');
+	loop++;
+}
 
 	return(0);
 }
--- CH04/04_14/04_14-challenge1.c
+++ CH04/04_14/04_14-challenge1.c
@@ -7,13 +7,46 @@
 int main()
 {
 	// variable declarations
-			
+	char command;
+
 	/* main program loop */
-	done = FALSE;
-	while(!done)
+	int done = FALSE;
+	while (!done)
 	{
 		// prompt for input
+		printf("Command: ");
+		scanf(" %c",&command);
 		// process input
+		switch (command)
+		{
+		case '?':
+		case 'h':
+		case 'H':
+			printf("R is input, output \"move right\"");
+			printf("\nL is input, output \"move left\"");
+			printf("\nB is input, output \"move back\"");
+			printf("\nQ is input, break the loop");
+			printf("\nH or ? is input, output valid commands\n");
+			break;
+		case 'R':
+		case 'r':
+			printf("move right\n");
+			break;
+		case 'L':
+		case 'l':
+			printf("move left\n");
+			break;
+		case 'B':
+		case 'b':
+			printf("move Back\n");
+			break;
+		case 'Q':
+		case 'q':
+			done = TRUE;
+			break;
+		default:
+			printf("identify invalid input\n");
+		}
 		// if R is input, output "move right"
 		// if L is input, output "move left"
 		// if B is input, output "move back"
